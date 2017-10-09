d7b21eb7cec0b21a243d8437998d8b9265192f65
Nagashayan <nagashayan1@gmail.com>
2017-10-08 20:15:31 -0400

    list out features from server

--- featuremodel.js
+++ featuremodel.js
@@ -1,16 +1,22 @@
 function Feature(data) {
+    console.log(data.description);
+    this.client_id = ko.observable(data.client_id);
     this.title = ko.observable(data.title);
+    this.description = ko.observable(data.description);
+    this.product_area = ko.observable(data.product_area);
+    this.target_date = ko.observable(data.target_date);
 }
 
 function FeatureListViewModel() {
     // Data
     var self = this;
-    self.title = "naga"
+    
     self.features = ko.observable();
    
     // Load initial state from server, convert it to Feature instances, then populate self.Features
     $.getJSON("//127.0.0.1:5000/feature/", function(allData) {
         var mappedFeatures = $.map(allData, function(item) { return new Feature(item) });
+        console.log(mappedFeatures);
         self.features(mappedFeatures);
     });    
 }
