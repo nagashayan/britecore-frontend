732d4cef1973517a8f6e831d09e173b1c49cff59
Nagashayan <nagashayan1@gmail.com>
2017-10-16 19:14:52 -0400

    add comments and format date & source code

--- featuremodel.js
+++ featuremodel.js
@@ -29,10 +29,10 @@ function FeatureListViewModel() {
     // Load initial state from server, convert it to Feature instances, then populate self.Features
     $.getJSON("//127.0.0.1:5000/feature/", function (allData) {
         var mappedFeatures = $.map(allData, function (item) {
-            var tempFeature =  new Feature(item);
+            var tempFeature = new Feature(item);
             // formatting date similar to input
             var tempdate = new Date(tempFeature.target_date());
-            tempFeature.target_date = tempdate.getMonth() +"/"+ tempdate.getDate() +"/"+ tempdate.getFullYear();
+            tempFeature.target_date = tempdate.getMonth() + "/" + tempdate.getDate() + "/" + tempdate.getFullYear();
             return tempFeature;
         });
         console.log(mappedFeatures);
@@ -72,7 +72,8 @@ function FeatureListViewModel() {
         });
 
     };
-    self.updateBackend = function(feature){
+    //make backend call and update feature once user clicks on update button below form.
+    self.updateBackend = function (feature) {
         console.log("updating backend");
 
         //make call to backend on success destroy on frontend
@@ -94,7 +95,7 @@ function FeatureListViewModel() {
                     self.editFeature().description(self.newdescription());
                     self.editFeature().product_area(self.newproduct_area());
                     self.editFeature().target_date(self.newtarget_date());
-                      
+
                 },
                 error: function (err) {
                     // if form was not saved throw and log error
@@ -107,18 +108,22 @@ function FeatureListViewModel() {
         }
         self.cancelUpdate();
     };
-    //deletes the future
-    //waits for backend confirmation and then destroys frontend feature
+    //loads form with current  record data
+    //once the user clicks on update button next to record
     self.updateFeature = function (feature) {
         console.log(feature);
         self.editFeature(feature);
-        self.newclient_id(feature.client_id);
-        self.newtitle(feature.title);
-        self.newdescription(feature.description);
-        self.newproduct_area(feature.product_area);
-        self.newtarget_date(feature.target_date);
+        self.newclient_id(feature.client_id());
+        self.newtitle(feature.title());
+        self.newdescription(feature.description());
+        self.newproduct_area(feature.product_area());
+
+        //format date according to date widget
+        var tempdate = new Date(feature.target_date);
+        //feature.target_date = tempdate.getFullYear() + "-" + tempdate.getMonth() + "-" + tempdate.getDate();
+        self.newtarget_date(self.getFormattedDate(tempdate));
         self.updateForm(true);
-        
+
 
     };
     //deletes the future
@@ -153,23 +158,38 @@ function FeatureListViewModel() {
         }
 
     };
-    self.clearForm = function(){
+
+    self.clearForm = function () {
         //clear form after adding
-     self.newclient_id("");
-     self.newtitle("");
-     self.newdescription("");
-     self.newproduct_area("");
-     self.newtarget_date("");
+        self.newclient_id("");
+        self.newtitle("");
+        self.newdescription("");
+        self.newproduct_area("");
+        self.newtarget_date("");
     };
-   
+
+    // if user clicks on cancel before clicking save/update button
     self.cancelUpdate = function () {
         console.log("cancelling form");
         self.updateForm(false);
         self.clearForm();
 
-    }
+    };
+    // format date in MM-dd-yyyy
+    self.getFormattedDate = function (date) {
+        var year = date.getFullYear();
+
+        var month = (1 + date.getMonth()).toString();
+        month = month.length > 1 ? month : '0' + month;
+
+        var day = date.getDate().toString();
+        day = day.length > 1 ? day : '0' + day;
+
+        return year + '-' + month + '-' + day;
+    };
 }
 
 $(document).ready(function () {
+    //initial binding
     ko.applyBindings(new FeatureListViewModel());
-  });
+});
